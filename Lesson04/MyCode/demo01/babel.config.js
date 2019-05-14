042ec7d0e59de77e3b87cba466475485fe5c7a14
RyzeZhao <643080112@qq.com>
2019-05-14 15:45:56 +0800

    4.1.2.2    不同情况二：如果该依赖交付了 ES5 代码并显式地列出了需要的 polyfill（就是这个依赖有使用ES环境或要求使用polyfill）      你可以使用 @vue/babel-preset-app 的 polyfills 选项预包含所需要的 polyfill。注意 es6.promise 将被默认包含，因为现在的库依赖 Promise 是非常普遍的。以下就用es6.promise演示，只需要在babel.config.js中添加即可

--- Lesson04/MyCode/demo01/babel.config.js
+++ Lesson04/MyCode/demo01/babel.config.js
@@ -1,5 +1,11 @@
+//加入垫片
 module.exports = {
-  presets: [
-    '@vue/app'
-  ]
+    presets: [
+        ['@vue/app', {
+            polyfills: [
+                'es6.promise',
+                'es6.symbol'
+            ]
+        }]
+    ]
 }
