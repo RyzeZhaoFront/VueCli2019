8a57da4bc4a98d84d5ddd2d92a3a08e041890b3a
RyzeZhao <643080112@qq.com>
2019-05-19 10:21:41 +0800

    ​5.1.3.2.2    ​新建vue.config.js ​

---
+++ Lesson05/MyCode/demo01/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    chainWebpack: config => {
+        // 输出删除前的config插件数据
+        console.log(config.plugins);
+        //删除prefetch和preload插件
+        config.plugins.delete('prefetch');
+        config.plugins.delete('preload');
+        //用于分隔
+        console.log("===================");
+        //再次输出config插件数据
+        console.log(config.plugins);
+    }
+}
