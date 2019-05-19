07e545b81858acfe37c91f3bcb19697e2e7450c6
RyzeZhao <643080112@qq.com>
2019-05-19 10:59:13 +0800

    5.1.5.2.2    ​修改vue.config.js

--- Lesson05/MyCode/demo01/vue.config.js
+++ Lesson05/MyCode/demo01/vue.config.js
@@ -1,15 +1,9 @@
 module.exports = {
-    chainWebpack: config => {
-        // 输出删除前的config插件数据
-        console.log(config.plugins);
-        //不生成html
-        config.plugins.delete('html');
-        //删除prefetch和preload插件
-        config.plugins.delete('prefetch');
-        config.plugins.delete('preload');
-        //用于分隔
-        console.log("===================");
-        //再次输出config插件数据
-        console.log(config.plugins);
+    pages: {
+        index: {
+            entry: 'src/pages/home/index.js', template: 'src/pages/home/index.html',
+        }, products: {
+            entry: 'src/pages/products/index.js', template: 'src/pages/products/index.html',
+        },
     }
 }
