1062ff9fde91eda393ebe44de97355f115f35ca0
RyzeZhao <643080112@qq.com>
2019-05-19 10:37:35 +0800

    5.1.4.1    ​vue.config.js加入这句

--- Lesson05/MyCode/demo01/vue.config.js
+++ Lesson05/MyCode/demo01/vue.config.js
@@ -2,6 +2,8 @@ module.exports = {
     chainWebpack: config => {
         // 输出删除前的config插件数据
         console.log(config.plugins);
+        //不生成html
+        config.plugins.delete('html');
         //删除prefetch和preload插件
         config.plugins.delete('prefetch');
         config.plugins.delete('preload');
